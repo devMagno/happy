477731c81958aac7b91cc27b78782cc2cd32c5bc
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-13 15:41:40 -0300

    Configura mapa, ícone e popup do Leaflet

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,19 @@
+let map = L.map('mapid').setView([-23.9618, -46.3322], 15)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+L.marker([-23.9618, -46.3322], { icon }).addTo(map)
+    .bindPopup(popup)
