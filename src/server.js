087dd8999741eae1b7f767b19ebd1fc266ae4447
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-16 19:20:19 -0300

    Configura template engines

--- src/server.js
+++ src/server.js
@@ -3,4 +3,12 @@ const path = require('path')
 
 const server = express()
 
+server
+  .use(express.static('public'))
+  .set('views', path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+  .get('/', (request, response) => {
+    return response.render('index')
+  })
+
 server.listen(5500)
