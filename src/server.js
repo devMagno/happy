65f3fa785ac2807ef6a637ec02f8a715632fd257
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-16 19:41:00 -0300

    Importa pages.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const pages = require('./pages')
 
 const server = express()
 
@@ -7,8 +8,6 @@ server
   .use(express.static('public'))
   .set('views', path.join(__dirname, "views"))
   .set('view engine', 'hbs')
-  .get('/', (request, response) => {
-    return response.render('index')
-  })
+  .get('/', pages.index)
 
 server.listen(5500)
