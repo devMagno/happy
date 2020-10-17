b851a17ef832a1094f75160305e70b1fbfad8a8e
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-16 23:50:34 -0300

    Adiciona configuração dos marcadores no mapa de orfanatos

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -8,12 +8,30 @@ const icon = L.icon({
   iconAnchor: [29, 68],
   popupAnchor: [170, 2]
 })
-const popup = L.popup({
-  closeButton: false,
-  className: 'map-popup',
-  minWidth: 240,
-  minHeight: 240
-}).setContent('Lar das Meninas <a href="/orphanage?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>')
 
-L.marker([-23.9618, -46.3322], { icon }).addTo(map)
-    .bindPopup(popup)
+function addMarker({id, name, lat, lng}) {
+
+  const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+  }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"></a>`)
+
+  L.marker([lat, lng], { icon })
+      .addTo(map)
+      .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('div.orphanages span')
+console.log(orphanagesSpan)
+orphanagesSpan.forEach( span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+
+  addMarker(orphanage)
+});
