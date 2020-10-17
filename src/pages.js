ccbdba4f0e2fb52fd4fd87c91d9f885f825adeee
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-17 16:54:24 -0300

    Configura rota da página do orfanato, passando os dados do orfanato

--- src/pages.js
+++ src/pages.js
@@ -18,8 +18,18 @@ module.exports = {
     }
   },
 
-  orphanage(req, res){
-    return res.render('orphanage')
+  async orphanage(req, res){
+    const id = req.query.id
+
+    try {
+      const db = await Database
+      const orphanage = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+
+      return res.render('orphanage', { orphanage: orphanage[0] })
+    } catch (error) {
+      console.log(error)
+      return res.send('Erro no banco de dados!')
+    }
   },
 
   createOrphanage(req, res){
