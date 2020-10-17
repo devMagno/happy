2d4c2425829729857d289645feccf49a58943ba3
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-17 16:20:53 -0300

    Remove função de inserir dados para outro arquivo

--- src/database/test.js
+++ src/database/test.js
@@ -1,34 +1,30 @@
 const Database = require('./db.js')
+const saveOrphanage = require('./saveOrphanage.js')
 
 Database.then(async db => {
-  await db.run(`
-    INSERT INTO orphanages (
-      lat,
-      lng,
-      name,
-      about,
-      whatsapp,
-      images,
-      instructions,
-      opening_hours,
-      open_on_weekends
-    ) VALUES (
-      "-23.938663322436103",
-      "-46.36931061744691",
-      "Lar das Meninas",
-      "Presta assistência à crianças de 4 a 17 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
-      "13991828585",
-      "https://images.unsplash.com/photo-1601950293362-651f3b3cc35f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
-      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-      "Das 6h até 18h",
-      "0"
-    );
-  `)
+  await saveOrphanage(db, {
+    lat: '-23.938663322436103',
+    lng: '-46.36931061744691',
+    name: 'Orfanato Raio de Sol',
+    about: 'Presta assistência à crianças de 4 a 17 anos que se encontram em situação de risco e/ou vulnerabilidade social.',
+    whatsapp: '13991828585',
+    images: [
+      "https://images.unsplash.com/photo-1601272792584-e3707a8b3cd7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601221780544-713899476f52?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601212173774-453efa102624?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1600728611365-b8fc376080a8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601897186210-844903d75623?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601841448425-c4ab33930440?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
+    ].toString(),
+    instructions: 'Venha como se sentir a vontade e traga muito amor e paciência para dar.',
+    opening_hours: 'Das 7h até 21h',
+    open_on_weekends: '1'
+  })
 
   const selectedOrphanages = await db.all("SELECT * FROM orphanages")
   console.log(selectedOrphanages)
 
-  const selectedOrphanage = await db.all('SELECT * from ORPHANAGES WHERE id = "2"')
+  const selectedOrphanage = await db.all('SELECT * from ORPHANAGES WHERE id = "3"')
   console.log(selectedOrphanage)
 
 })
