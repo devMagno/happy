9acd7820eba5fed448a901fa38010bde4b98819f
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-16 21:14:42 -0300

    Adiciona dados fictícios de orfanatos

---
+++ src/database/fake-data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: 1,
+    lat: '-23.9618',
+    lng: '-46.3322',
+    name: 'Lar das Meninas',
+    description: 'Presta assistência à crianças de 6 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      "https://images.unsplash.com/photo-1601950293362-651f3b3cc35f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1600469985996-89f760545e99?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1602407824187-5dee9030e31a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1600257254304-2da1f97b83e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1602660126348-8d582be369ea?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1602720198576-594031bb2615?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
+    ],
+    instructions: 'Venha como se sentir a vontade e traga muito amor e paciência para dar.',
+    opening: 'Das 8h até 8h',
+    open_on_weekends: '1'
+  },
+  {
+    id: 2,
+    lat: '-23.938663322436103',
+    lng: '-46.36931061744691',
+    name: 'Lar dos Meninos',
+    description: 'Presta assistência à crianças de 4 a 17 anos que se encontram em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      "https://images.unsplash.com/photo-1601272792584-e3707a8b3cd7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601221780544-713899476f52?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601212173774-453efa102624?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1600728611365-b8fc376080a8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601897186210-844903d75623?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1601841448425-c4ab33930440?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
+    ],
+    instructions: 'Venha como se sentir a vontade e traga muito amor e paciência para dar.',
+    opening: 'Das 7h até 20h',
+    open_on_weekends: '0'
+  }
+]
