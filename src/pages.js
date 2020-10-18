435eab43d7b5c9cacf7e0546a03741c48fa2e95b
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-17 23:00:49 -0300

    Configura rota para salvar orfanato cadastrado

--- src/pages.js
+++ src/pages.js
@@ -40,6 +40,35 @@ module.exports = {
 
   createOrphanage(req, res){
     return res.render('create-orphanage')
+  },
+
+  async saveOrphanage(req, res){
+    const fields = req.body
+
+    if(Object.values(fields).includes('')){
+      return res.send('Todos os campos devem ser preenchidos!')
+    }
+
+
+    try {
+      const db = await Database
+      await saveOrphanage(db, {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp,
+        images: fields.images.toString(),
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends,
+      })
+
+      return res.redirect('/orphanages')
+    } catch (error) {
+      console.log(error)
+      return res.send('Erro no banco de dados!')
+    }
   }
 
 }
